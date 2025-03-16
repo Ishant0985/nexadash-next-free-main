4a7650aff3cb07becfa61a65489417f1e484610a
Ishant0985 <yadav.ishantsbi0985@gmail.com>
2025-03-16 21:33:58 +0530

    t

--- app/(auth)/invoice/view/[id]/page.tsx
+++ app/(auth)/invoice/view/[id]/page.tsx
@@ -1,6 +1,11 @@
+'use client';
+
+import { useParams } from 'next/navigation';
 import InvoiceViewContent from './invoice-view-content';
 
-// Make the component async to match Next.js App Router typing requirements
-export default async function InvoiceViewPage({ params }: { params: { id: string } }) {
-  return <InvoiceViewContent invoiceId={params.id} />;
+export default function InvoiceViewPage() {
+  const params = useParams();
+  const invoiceId = Array.isArray(params?.id) ? params.id[0] : params?.id as string;
+  
+  return <InvoiceViewContent invoiceId={invoiceId} />;
 }
